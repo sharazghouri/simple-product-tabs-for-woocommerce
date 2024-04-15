11794dfb4b8c7b8ae8bd9893b24a37fb5d6d246c
Sharaz Shahid <sharazghouri235@outlook.com>
2024-04-15 19:32:09 +0100

    settings init

---
+++ assets/css/admin.asset.php
@@ -0,0 +1,4 @@
+<?php return array(
+	'dependencies' => array(),
+	'version'      => '36dcbcd2c4f29281674e',
+);
---
+++ assets/js/admin.asset.php
@@ -0,0 +1,4 @@
+<?php return array(
+	'dependencies' => array(),
+	'version'      => '4813bb92c27ef6310116',
+);
